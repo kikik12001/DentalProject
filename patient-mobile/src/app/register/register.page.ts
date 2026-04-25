9efc70edf11e9f06b50d8132073a792cafc85cfa
kikik12001 <eunji20070@gmail.com>
2026-04-25 21:29:15 +0100

    created register page

--- patient-mobile/src/app/register/register.page.ts
+++ patient-mobile/src/app/register/register.page.ts
@@ -1,20 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+import {Auth, createUserWithEmailAndPassword} from '@angular/fire/auth';
+import {Firestore, doc, setDoc} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage  {
 
-  constructor() { }
+  patientData = {
 
-  ngOnInit() {
+    name: '',
+    dob: '',
+    phone: '',
+    address: '',
+    email: '',
+    password: '',
+    allergies: '',
+    pps: ''
+   };
+  
+
+  constructor(
+    private auth: Auth,
+    private firestore: Firestore
+  ) { }
+
+  async registerPatient() {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        this.auth, 
+        this.patientData.email, 
+        this.patientData.password);
+
+        const userID = userCredential.user.uid;
+
+        const userDocRef = doc(this.firestore, `Users/${userID}`);
+
+        await setDoc(userDocRef, {
+          name: this.patientData.name,
+          dob: this.patientData.dob,
+          phone: this.patientData.phone,
+          address: this.patientData.address,
+          email: this.patientData.email,
+          allergies: this.patientData.allergies,
+          pps: this.patientData.pps,
+          role: 'patient'
+         });
+        
+         alert('Account created successfully!');
+
+    } catch (error:any) {
+      console.error('Error registering:', error);
+      alert('Registration failed: ' + error.message);
+    }
+  }
   }
 
-}
+
