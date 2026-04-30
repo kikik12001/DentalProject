4daaf8082723bd465bf67a374e392900679d658f
kikik12001 <eunji20070@gmail.com>
2026-04-30 17:53:36 +0100

    connect my app to my firebase project

--- patient-mobile/src/main.ts
+++ patient-mobile/src/main.ts
@@ -8,11 +8,15 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "dentalapp-9bea0", appId: "1:295058520569:web:317e5c831a1f60181d6528", storageBucket: "dentalapp-9bea0.firebasestorage.app", apiKey: "AIzaSyBGYxQ25Hw1L82iLPAeay6rpvY49dBgs3s", authDomain: "dentalapp-9bea0.firebaseapp.com", messagingSenderId: "295058520569", measurementId: "G-7P2QW7ZD00"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()),
-  ],
+    provideRouter(routes),
+    //this connects app to my firebase project
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ]
 });
